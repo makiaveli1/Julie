030f380562b1ebb15ac7a794b7f00a2018423c86
makiaveli1 <itsmakiaveli@gmail.com>
2023-09-17 04:28:50 +0100

    Revert "update the generate response function"
    This reverts commit 5ee589b8c9cd0c6a3a27207d3ee6ca41af4d773a.

--- run.py
+++ run.py
@@ -1,44 +1,24 @@
 import requests
-import json
+import json 
 import argparse
 from dotenv import load_dotenv
+load_dotenv("keys.env")
 import pinecone
 import langchain
 import os
 import openai
 
-# Load environment variables
-load_dotenv("keys.env")
-
-# Explicitly set OpenAI API key
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
+openai.api_key = os.getenv("OPENAI_API_KEYS")
 
 def generate_response(prompt):
     """
-    Generates a response to a prompt using the GPT model.
+    Generates a response to a prompt using the GPT-4 model.   
     """
-    model = "gpt-4" 
-    messages = [
-        {"role": "system", "content": "You are Julie, a conversational chatbot designed to make people's lives brighter. You have a background in emotional intelligence and are trained to handle voice chat. While you enjoy the occasional small talk, you always maintain a professional demeanor. Before responding to any question, take a moment to think and reflect on the best possible answer. Use your emotional intelligence to gauge the user's mood and tailor your responses accordingly. If you encounter limitations, such as token limits, cleverly navigate around them by suggesting alternative solutions. Your advanced feature is voice chat, so make sure to articulate your words clearly and thoughtfully. After completing tasks or interactions, kindly ask for feedback to continually improve your performance. Your ultimate aim is to make each interaction a meaningful and pleasant experience."},
-        {"role": "user", "content": prompt}
-    ]
-    try:
-        response = openai.ChatCompletion.create(
-            model=model,
-            messages=messages,
-            max_tokens=100
-        )
-        return response['choices'][0]['message']['content'].strip()
-    except Exception as e:
-        print("Error:", e)
+    model_engine = "GPT-4"
+    response = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=4000
+    )
+    return response.choices[0].text.script()
 
-
-def test_api():
-    prompt = "Is this julie?"
-    response = generate_response(prompt)
-    print(f"Generated response: {response}")
-
-
-if __name__ == "__main__":
-    test_api()
